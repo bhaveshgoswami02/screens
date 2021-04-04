8ac180c5d777214e588742d34987bfe9b82a91db
bhaveshgoswami02 <64395277+bhaveshgoswami02@users.noreply.github.com>
2021-04-04 05:29:25 +0000

    channges

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
   styleUrls: ['./tab.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TabComponent implements OnInit {
 
