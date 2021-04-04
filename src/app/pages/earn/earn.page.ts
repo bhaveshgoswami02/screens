aaf9224d53ab7c9f5bcd2c356d317a488bc50f2c
bhaveshgoswami02 <64395277+bhaveshgoswami02@users.noreply.github.com>
2021-04-04 06:54:55 +0000

    chamges

--- src/app/pages/earn/earn.page.ts
+++ src/app/pages/earn/earn.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-earn',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EarnPage implements OnInit {
 
-  constructor() { }
+  constructor(public router:Router) { }
 
   ngOnInit() {
   }
 
+  nextPage() {
+    console.log("run")
+    this.router.navigateByUrl("/earn-detail")
+  }
 }
